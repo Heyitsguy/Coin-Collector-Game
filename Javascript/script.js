f299d77413680bb8ba68ac541e2926f0808330a9
Andy Wang <wang.andy32@gmail.com>
2018-07-26 11:06:16 -0400

    changed wario src, added local highscore and wario now follows you on the y axis

--- Javascript/script.js
+++ Javascript/script.js
@@ -8,21 +8,46 @@ var game = new Phaser.Game(640, 480, Phaser.AUTO, 'stage', {
 // Global Variables
 var cursors;
 var player_speed = 600;
-
+var hscore=0;
 var coin;
 var coin_padding = 60;
 var score = 0;
 var score_text;
 var gameOverText;
 var enemySpeed=400;
+var hscoreText="";
+var newRecord=false;
+var r;
+var resetText;
 
 //Global functions
 
-var randomCoinPoisition = function(){
+var randomCoinPosition = function(){
   let posX = Math.round(Math.random()*(game.width-(2*coin_padding+1)+coin_padding));
   let posY = Math.round(Math.random()* (game.height-(2*coin_padding+1)+coin_padding));
   coin.position = new Phaser.Point(posX,posY);
 }
+
+function checkHScore(){
+  if (score>hscore){
+    newRecord = true;
+    hscore = score;
+    hscoreText.text = "High Score: " + hscore;
+    localStorage.setItem('hscore', hscore);
+  }else{
+    newRecord = false;
+  }
+}
+
+function reset(){
+  score = 0;
+  score_text.text = 'Score:'+ score;
+  gameOverText.text="";
+  resetText.text = "";
+  player.reset(128,128);
+  randomCoinPosition();
+}
+
 
 // var Collect = function(){
 //   randomCoinPoisition();
@@ -36,7 +61,7 @@ function preload() {
     game.load.image('player','https://heyitsguy.github.io/Coin-Collector-Game/Images/Mariopix.png');
 
     game.load.image('coin', 'https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg');
-    game.load.image('enemy', '../Images/Wario.png');
+    game.load.image('enemy', 'https://heyitsguy.github.io/Coin-Collector-Game/Images/Wario.png');
 
 }
 
@@ -67,13 +92,29 @@ function create() {
 
     gameOverText.setTextBounds(0,0,640,480);
 
+    if(localStorage.getItem('hscore')){
+      hscore = localStorage.getItem('hscore');
+    }
+
+    r=game.input.keyboard.addKey(Phaser.Keyboard.R);
+
+    resetText = game.add.text(0,0,"",{
+      fill: 'white',
+      boundsAlignH:'center',
+      boundsAlignV: 'bottom'
+    });
+    resetText.setTextBounds(0,0,640,480);
+
+    hscoreText= game.add.text(440,15, "High Score: " + hscore, {
+      fill: 'white'
+    });
     // Add Key
     coin = game.add.sprite(200, 200, 'coin'); // Add key sprite at x, y position
     coin.scale.setTo(0.4, 0.4); //scale the image
     game.physics.enable(coin, Phaser.Physics.ARCADE); // Enable key physics
     coin.body.immovable = true; // Keep key from moving
     // Picks a random location on the stage for the coin to appear.
-    randomCoinPoisition();
+    randomCoinPosition();
     score_text = game.add.text(15,15,"Score:" + score,{
       fill:"white"
     });
@@ -81,6 +122,9 @@ function create() {
 
 // Runs every frame
 function update() {
+  if (r.downDuration(1)) {
+  reset();
+  }
     var speed_x = 0; // Current x speed
     var speed_y = 0; // Current y speed
 
@@ -101,20 +145,26 @@ function update() {
     var speed_dir = new Phaser.Point(speed_x, speed_y); // make speed vector
     player.body.velocity = speed_dir; // Set player velocity
 
-    var enemyDir = new Phaser.Point(enemySpeed,0);
+    var enemyDir = new Phaser.Point(enemySpeed,(speed_y * .5));
     if(enemy.world.x > 640){
       enemy.position = new Phaser.Point(0,player.world.y)
     }
     enemy.body.velocity = enemyDir;
 
     game.physics.arcade.collide(player, coin, function(){
-      randomCoinPoisition();
+      randomCoinPosition();
       score++;
       score_text.text = "Score: " +score;
       document.getElementById("score").innerHTML = score;
+      checkHScore();
     }); // Player-key collision
     game.physics.arcade.collide(player, enemy, function(){
       player.kill();
       gameOverText.text = "GAME OVER";
+      resetText.text = "Hit the R key to Restart!";
     });
+      if(newRecord === true){
+        gameOverText.text += "\n New Record!";
+      }
+
 }
